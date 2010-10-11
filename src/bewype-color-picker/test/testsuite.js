7f3a9b584a53d501fa8496129d3dab4b7acd4454
florent <florent@florent-laptop.(none)>
2010-10-11 16:48:07 +0200

    test uncomment

--- src/bewype-color-picker/test/testsuite.js
+++ src/bewype-color-picker/test/testsuite.js
@@ -13,7 +13,7 @@ suite.add( new Y.Test.Case( {
     tearDown: function () {
         // unplug
         if ( this._picker ) {
-            //this._picker.destroy();
+            this._picker.destroy();
         }
     },
 
