0e59dfa0022859853f1684f8df263d9794ec45b3
florent <florent@florent-laptop.(none)>
2010-10-12 18:37:37 +0200

    add button picker test

--- src/bewype-button/test/testsuite.js
+++ src/bewype-button/test/testsuite.js
@@ -49,10 +49,10 @@ suite.add( new Y.Test.Case( {
     "test button toggle": function () {
 
         // tmp vars
-        var _evtNode = null,
-            _clicked = false,
-            _changed = false
-            _class   = null;
+        var _evtNode    = null,
+            _clicked    = false,
+            _changed    = false,
+            _class      = null;
 
         // init normal button
         this._button = new Y.Bewype.ButtonToggle();
@@ -71,7 +71,7 @@ suite.add( new Y.Test.Case( {
         } );
 
         // add custom event listener
-        this._button.on( 'button:onClick', function ( e ) {
+        this._button.on( 'button:onChange', function ( e ) {
             _changed = true;
         } );
         
@@ -90,11 +90,66 @@ suite.add( new Y.Test.Case( {
         // check
         Y.Assert.isFalse( this._button.getValue(), '(re)click toggle value' );
         Y.Assert.areEqual( _class, _evtNode.get( 'className' ), '(re)click toggle class' );
+    },
+
+    "test button picker": function () {
+
+        // tmp vars
+        var _evtNode = null,
+            _clicked = false,
+            _changed = false,
+            _class   = null,
+            _pickerNode = null;
+
+        // init normal button
+        this._button = new Y.Bewype.ButtonPicker( {
+                label     : 'Font Size',
+                pickerObj :  Y.Bewype.PickerFontSize
+        } );
+        _class = this._button.get ( 'buttonClass' );
+
+        // check exist
+        Y.Assert.isNotNull( this._button, 'Ooops!' );
+        Y.Assert.isNotUndefined( this._button, 'Ooops!' );
+
+        // render
+        this._button.render( '#test' );
+
+        // add custom event listener
+        this._button.on( 'button:onClick', function ( e ) {
+            _clicked = true;
+        } );
+
+        // add custom event listener
+        this._button.on( 'button:onChange', function ( e ) {
+            _changed = true;
+        } );
+        
+        // click!
+        _evtNode = Y.one( '.' + _class );
+        _evtNode.simulate( 'click' );
+
+        // picker node should exist
+        _pickerNode = Y.one( '.' + _class + '-picker-host' );
+        Y.Assert.isNotNull( _pickerNode, 'no picker node!' );
+        
+        // select 12
+        _evtNode = Y.one( '#' + this._button._picker.get( 'pickerClass' ) + '-12' );
+        _evtNode.simulate( 'click' );
+
+        // check click
+        Y.Assert.isTrue( _clicked, 'Ooops!' );
+        Y.Assert.isTrue( _changed, 'Ooops!' );
+        Y.Assert.areEqual( '12', this._button.getValue(), 'click picker value' );
+
+        // picker node should be undefined
+        _pickerNode = Y.one( '.' + _class + '-picker-host' );
+        Y.Assert.isNull( _pickerNode, 'a picker node!' );
     }
 
 } ) );
 
 Y.Test.Runner.add( suite );
 
-}, '@VERSION@' ,{ requires: [ 'node-event-simulate', 'test', 'bewype-button' ] } );
+}, '@VERSION@' ,{ requires: [ 'node-event-simulate', 'test', 'bewype-button', 'bewype-picker' ] } );
 
