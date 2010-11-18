5e226e7edf1a40d1a1376410e1f2a92d289f7b1c
florent <florent@florent-laptop.(none)>
2010-11-18 17:39:01 +0100

    do some editor enhancement ...

--- build/bewype-editor/bewype-editor-text.js
+++ build/bewype-editor/bewype-editor-text.js
@@ -5,6 +5,7 @@ YUI.add('bewype-editor-text', function(Y) {
      *
      */
     var PLACE_TMPL   = '',
+        IMAGE_TMPL   = '',
         EditorText   = null;
 
     /**
@@ -12,6 +13,11 @@ YUI.add('bewype-editor-text', function(Y) {
      */
     PLACE_TMPL += '<div class="{editorClass}-place">';
     PLACE_TMPL += '</div>';
+
+    /**
+     *
+     */
+    IMAGE_TMPL += '<img src="{filePath}" />';
 
     /**
      *
@@ -53,11 +59,12 @@ YUI.add('bewype-editor-text', function(Y) {
             var _host        = this.get( 'host'        ),
                 _editorClass = this.get( 'editorClass' ),
                 _cssDict     = Y.Bewype.Utils.getCssDict( _host ),
-                _placeNode   = null,
-                _main        = new Y.Node.create( '<div><div class="main" /></div>' );
+                _innerHTML   = _host.get( 'innerHTML' ),
+                _placeNode   = null;
+                // _main        = new Y.Node.create( '<div><div class="main" /></div>' );
 
             // set content to main
-            _main.one( '.main' ).append( this.getInnerHTML( _host ) );
+            // _main.one( '.main' ).append( this.getInnerHTML( _host ) );
             
             // and empty tag content temporary
             _host.set( 'innerHTML', '' );
@@ -73,7 +80,7 @@ YUI.add('bewype-editor-text', function(Y) {
 
             // Create the Base Editor
             this._editor = new Y.EditorBase( {
-                content : _main.get( 'innerHTML' )
+                content : _innerHTML // _main.get( 'innerHTML' )
             } );
 
             // Rendering the Editor
@@ -92,13 +99,10 @@ YUI.add('bewype-editor-text', function(Y) {
             var _host          = this.get( 'host' ),
                 _placeClass    = this.get( 'editorClass' ) + '-place',
                 _placeNode     = _host.one( '.' + _placeClass ),
-                _body          = this._editor.getInstance().one( 'body' ),
-                _main          = _body.one( '.main' ),
+                _inst          = this._editor.getInstance(),
+                _body          = _inst.one( 'body' ),
                 _fn            = null,
                 _spinnerValues = {};
-
-            // ensure nice body
-            _body.setStyle( 'cssText', 'padding: 0; margin: 0; height: 100%; width: 100%;');
 
             _fn = function ( key, val ) {
 
@@ -115,7 +119,7 @@ YUI.add('bewype-editor-text', function(Y) {
                     _spinnerValues[ key ] = val;
                 } else if ( key ) {
                     // style for the edited content
-                    _main.setStyle( Y.Bewype.Utils.camelize( key ), val );
+                    _body.setStyle( Y.Bewype.Utils.camelize( key ), val );
                 }
 
                 // return something to continue
@@ -125,8 +129,18 @@ YUI.add('bewype-editor-text', function(Y) {
             // ensure height width
             this._oMainCssdict.height = '100%';
             this._oMainCssdict.width  = '100%';
-            _main.setStyle( 'height', '100%' );
-            _main.setStyle( 'width' , '100%' );
+
+            Y.each( [ 'iframe', 'html', 'body' ] , function ( v, k ) {
+                // ensure nice rendering
+                var _t = _host.one( v ) || _inst.one( v );
+                _t.setStyle( 'cssText', 'padding: 0px; margin: 0px; height: 100%; width: 100%;');
+
+                // specific for chrome
+                if ( Y.UA.chrome && v === 'iframe' ) {
+                    _t.setStyle( 'margin-top', '-15px' );
+                }
+
+            } );
 
             // separate place and content style
             Y.JSON.stringify( cssDict, Y.bind( _fn, this ) );
@@ -165,8 +179,8 @@ YUI.add('bewype-editor-text', function(Y) {
                 _placeClass    = '.' + _editorClass + '-place',
                 _placeNode     = _host.one( _placeClass  ),
                 _body          = this._editor.getInstance().one( 'body' ),
-                _main          = _body.one( '.main' ),
-                _selectionNode = _main.one( '.selection' ),
+                // _main          = _body.one( '.main' ),
+                _selectionNode = _body.one( '.selection' ),
                 _fn            = null;
 
             // hide first
@@ -178,7 +192,7 @@ YUI.add('bewype-editor-text', function(Y) {
             }
 
             // restore content
-            _host.set( 'innerHTML', _main.get( 'innerHTML' ) );
+            _host.set( 'innerHTML', _body.get( 'innerHTML' ) );
 
             // css spliter
             _fn = function ( type_, key, val ) {
@@ -195,7 +209,7 @@ YUI.add('bewype-editor-text', function(Y) {
             };
             // separate place and content style
             Y.JSON.stringify( Y.Bewype.Utils.getCssDict( _placeNode ), Y.bind( _fn, this, 'place'   ) );
-            Y.JSON.stringify( Y.Bewype.Utils.getCssDict( _main      ), Y.bind( _fn, this, 'content' ) );
+            Y.JSON.stringify( Y.Bewype.Utils.getCssDict( _body      ), Y.bind( _fn, this, 'content' ) );
 
             // destroy editor
             this._editor.destroy();
@@ -218,7 +232,7 @@ YUI.add('bewype-editor-text', function(Y) {
                 _node = _host.one( '.' + this.get( 'editorClass' ) + '-place' );
             } else {
                 // get content node
-                _node = this._editor.getInstance().one( '.main' );
+                _node = this._editor.getInstance().one( 'body' ); // .one( '.main' );
             }
 
             // do update
@@ -237,16 +251,16 @@ YUI.add('bewype-editor-text', function(Y) {
 
             if ( removeSelectionNode !== false ) {
                 var _inst = this._editor.getInstance(),
-                    _main = _inst.one( 'body' ).one( '.main' );
+                    _body = _inst.one( 'body' ); // _main = _inst.one( 'body' ).one( '.main' );
 
-                this.removeTagOrStyle( _main, '.selection' );
+                this.removeTagOrStyle( _body, '.selection' );
             }
         },
 
         _isFocusNodeFisrt : function ( selection ) {
 
             var _inst             = this._editor.getInstance(),
-                _mNode            = _inst.one( 'body' ).one( '.main' )._node,
+                _mNode            = _inst.one( 'body' )._node, // .one( '.main' )
                 _aNode            = selection.anchorNode,
                 _fNode            = selection.focusNode,
                 _aOffset          = selection.anchorOffset,
@@ -275,13 +289,14 @@ YUI.add('bewype-editor-text', function(Y) {
         _ensureRangeFirstNode : function ( selection, range, firstNode ) {
 
             var _inst = this._editor.getInstance(),
-                _main     = _inst.one( 'body' ).one( '.main' ),
-                _mainNode = _main._node,
-                _cK       = 0;
+                // _main     = _inst.one( 'body' ).one( '.main' ),
+                _mNode    = _inst.one( 'body' )._node,
+                _cK       = 0,
+                _err      = null;
 
-            if ( firstNode.parentNode != _mainNode ) {
+            if ( firstNode.parentNode != _mNode ) {
                 try {
-                    while ( firstNode.parentNode && firstNode.parentNode != _mainNode ) {
+                    while ( firstNode.parentNode && firstNode.parentNode != _mNode ) {
                         firstNode = firstNode.parentNode;
                         // manage endless case :s
                         _cK += 1;
@@ -291,24 +306,26 @@ YUI.add('bewype-editor-text', function(Y) {
                     }
                     range.setStartBefore( firstNode );
                 } catch ( err ) {
+                    _err = err;
                     return this._clearSelection( selection, range );
                 }
             } else if ( selection.anchorOffset === 0 && !selection.anchorNode.previousSibling ) {
-                range.setStart( _mainNode, 0 );
+                range.setStart( _mNode, 0 );
             }
             return true;
         },
 
         _ensureRangeLastNode : function ( selection, range, lastNode ) {
 
-            var _inst     = this._editor.getInstance(),
-                _main     = _inst.one( 'body' ).one( '.main' ),
-                _mainNode = _main._node,
-                _cK       = 0;
+            var _inst   = this._editor.getInstance(),
+                // _main     = _inst.one( 'body' ).one( '.main' ),
+                _mNode  = _inst.one( 'body' )._node,
+                _cK     = 0,
+                _err    = null;
 
-            if ( lastNode.parentNode != _mainNode ) {
+            if ( lastNode.parentNode != _mNode ) {
                 try {
-                    while ( lastNode.parentNode && lastNode.parentNode != _mainNode ) {
+                    while ( lastNode.parentNode && lastNode.parentNode != _mNode ) {
                         lastNode = lastNode.parentNode;
                         // manage endless case :s
                         _cK += 1;
@@ -318,6 +335,7 @@ YUI.add('bewype-editor-text', function(Y) {
                     }
                     range.setEndAfter( lastNode );
                 } catch ( err ) {
+                    _err = err;
                     return this._clearSelection( selection, range );
                 }
             }
@@ -343,8 +361,8 @@ YUI.add('bewype-editor-text', function(Y) {
             var _host          = this.get( 'host' ),
                 _inst          = this._editor.getInstance(),
                 _body          = _inst.one( 'body'  ),
-                _main          = _body.one( '.main' ),
-                _selectionNode = _main.one( '.selection' ),
+                // _main          = _body.one( '.main' ),
+                _selectionNode = _body.one( '.selection' ),
                 _selection     = Y.Bewype.Utils.getSelection( _host ),
                 _range         = Y.Bewype.Utils.getRange( _selection ),
                 _firstNode     = null,
@@ -381,9 +399,9 @@ YUI.add('bewype-editor-text', function(Y) {
                 try {
                     // create new surrounding node                             
                     _selectionNode = Y.Node.create( '<span class="selection"></span>' );
-                    _main.append( _selectionNode );
+                    _body.append( _selectionNode );
                     // set range surrounding node
-                    _range.surroundContents( _main.one( '.selection' )._node );
+                    _range.surroundContents( _body.one( '.selection' )._node );
                 } catch ( err ) {
                     // Oops! clear all
                     return this._clearSelection( _selection, _range );
@@ -393,7 +411,7 @@ YUI.add('bewype-editor-text', function(Y) {
                 this._clearSelection( _selection, _range, false );
 
             } else {
-                this.removeTagOrStyle( _main, '.selection' );
+                this.removeTagOrStyle( _body, '.selection' );
             }
 
             // update button status
@@ -403,13 +421,54 @@ YUI.add('bewype-editor-text', function(Y) {
             this._refreshSelectionNode();
         },
 
+        _insertNodeToSelection : function ( node ) {
+
+            var _host      = this.get( 'host' ),
+                _window    = Y.Bewype.Utils.getWindow( _host ),
+                _document  = Y.Bewype.Utils.getDocument( _host ),
+                _selection = null, 
+                _range     = null;
+            
+            if ( _window._node.getSelection ) { // Firefox, Google Chrome ??? , Safari, Opera
+                // ...
+                _selection = _window._node.getSelection();
+                // ...
+                if (_selection.rangeCount > 0) {
+                    _range = _selection.getRangeAt (0);
+                    _range.insertNode ( node._node );
+                }
+            } else {  // Internet Explorer
+                // ...
+                _range = _document._node.selection.createRange();
+                _range.collapse (true);
+                _range.pasteHTML ( node._node );
+            }
+        },
+
         _onEditorChange : function ( e ) {
             //
-            if ( e.changedEvent.type == 'dblclick' ) {
+            if ( e.changedEvent.type === 'dblclick' ) {
 
-            } else if ( e.changedEvent.type == 'mouseup' ) {
+            } else if ( e.changedEvent.type === 'mouseup' ) {
                 this._updateSelection();
             }
+            /*
+            else if ( e.changedEvent.type === 'keyup' &&  e.changedEvent.charCode === 13 ) {
+                var _inst = this._editor.getInstance(),
+                    _body = _inst.one( 'body' ),
+                    _main = _body.one( '.main' );
+                // merge content just in case
+                _body.get( 'children' ).each( function ( v, k ) {
+                    if ( v != _main ) {
+                        // set node cotent to main
+                        _main.append( Y.Node.create( '<br />' ) );
+                        _main.append( this.getInnerHTML( v ) );
+                        // remove node
+                        v.remove();
+                    }
+                } );
+            }
+            */
         },
 
         onButtonClick : function ( name, e ) {
@@ -425,57 +484,62 @@ YUI.add('bewype-editor-text', function(Y) {
 
             var _inst          = this._editor.getInstance(),
                 _body          = _inst.one( 'body'  ),
-                _main          = _body.one( '.main' ),
-                _selectionNode = _main.one( '.selection' ),
+                // _main          = _body.one( '.main' ),
+                _selectionNode = _body.one( '.selection' ),
                 _button        = this._panel.getButton( name ),
                 _value         = _button ? _button.getValue() : null,
+                _img           = null,
                 _tag           = null,
                 _tagNode       = null,
-                _newNode       = null;             
+                _newNode       = null,
+                _caretNode     = null;
 
             if ( !_selectionNode ) {
 
                 if ( this._panel.isCssButton( name ) ) {
-
                     // style update
                     this.updateStyle( name );
+                    // fire custom event
+                    return true;
 
+                }  else if ( name === 'file' ) {
+                    // ...
+                    _img = Y.Node.create( Y.substitute( IMAGE_TMPL, {
+                            filePath : this.get( 'fileStaticPath' ) + _value
+                        } )
+                    );
+                    // ...
+                    this._insertNodeToSelection( _img );
                     // fire custom event
                     return true;
 
                 } else if ( name === 'reset') {
-
                     // reset main node
-                    this.resetStyle( _main );
-
+                    this.resetStyle( _body );
                     // restore original values and quit
-                    Y.Bewype.Utils.setCssDict( _main, this._oMainCssdict );
-
+                    Y.Bewype.Utils.setCssDict( _body, this._oMainCssdict );
                     // fire custom event
                     return true;
                 }
-
+            } else if ( name === 'file' ) {
+                // pass
             } else {
                 // get previous tag
                 _tag = this._panel.getWorkingTagName( name, true );
-
                 // do some cleaning
                 if ( _tag ) {
                     this.removeTagOrStyle( _selectionNode, _tag, name );
                 }
-                
                 // current tag
                 _tag = this._panel.getWorkingTagName( name );
-
+                // ...
                 if ( _value && ( _value === true || _value.trim() !== '' ) ) {
-
                     // create tag node
                     if ( name === 'url' ) {
                         _tagNode = Y.Node.create( '<a href="' + _value + '"></a>' );
                     } else {
                         _tagNode = Y.Node.create( '<' + _tag + '></' + _tag + '>' );
                     }
-
                     // update with css property
                     if ( this._panel.isCssButton( name ) ) {
                         _tagNode.setStyle( Y.Bewype.Utils.camelize( name ), _value);
@@ -483,32 +547,26 @@ YUI.add('bewype-editor-text', function(Y) {
                             _tagNode.setStyle( 'display', 'inline-block' );
                         }
                     }
-
                     // create surrounding node
                     _newNode = Y.Node.create( '<span class="selection"></span>' );
                     _newNode.append( _tagNode );
                     //
                     _newNode.one( _tag ).append( this.getInnerHTML( _selectionNode ) );
-
                     // update current selection
                     _selectionNode.replace(_newNode);
 
                 } else if ( name === 'reset' ) {
-                    
                     // do reset
                     this.resetStyle( _selectionNode, true );
-
                     // refresh buttons
                     this._panel.refreshButtons( _selectionNode, true );
                 }
 
                 // custom rendering
                 this._refreshSelectionNode();
-
                 // fire custom event
                 return true;
             }
-
             // default
             return false;
         },
